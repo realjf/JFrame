fd23d18b1503b87af4289b6030efaf150bf06840
chenjiefeng <chenjiefeng@4399inc.com>
2018-02-05 16:16:16 +0800

    入口修改

--- public/index.php
+++ public/index.php
@@ -1,17 +1,17 @@
 <?php
 
-define("JFRAME_PATH", dirname(__FILE__));
+define("JFRAME_PATH", dirname(__DIR__));
 define("JFRAME_CORE_PATH", JFRAME_PATH . '/core');
 define("JFRAME_APP_PATH", JFRAME_PATH . '/app');
 define("JFRAME_CONFIG_PATH", JFRAME_PATH . '/config');
 define("JFRAME_LIBRARY_PATH", JFRAME_PATH . '/library');
-
+define("JFRAME_RESOURCE_PATH", JFRAME_PATH . '/public');
 
 define('FRAME_ENV_IS_CLI', PHP_SAPI == 'cli');
 
 
-require_once 'vendor/autoload.php';
-require_once 'core/JFrame/Output.php';
+require_once JFRAME_PATH . '/vendor/autoload.php';
+require_once JFRAME_PATH . '/core/JFrame/Output.php';
 
 // Whoops Exception Handler
 $whoops = new \Whoops\Run;
